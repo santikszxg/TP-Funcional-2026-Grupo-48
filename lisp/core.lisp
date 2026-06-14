1d4d715e49bc6baafe8d948ae17be8e6543c3d40
santikszxg <santi.ortiz001@gmail.com>
2026-06-13 21:08:26 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -46,6 +46,23 @@
 (defun auditoria(color-anterior color-nuevo)
 			(format t "Tiempo ~A: La luz ha cambiado de ~A a ~A%" (-(get-universal-time)2208988800) color-anterior color-nuevo))
 
+
+#|;Requerimiento 4: Temporizador Automático;
+;; ========================================================
+;; FUNCIÓN: timer
+;; NATURALEZA: Pura (Dado un timestamp, siempre retorna el mismo color)
+;; ESTRATEGIA: Orden Superior (Implementada mediante mapcar y reduce)
+;; IMPACTO: No destructiva
+;; ======================================================== |#
+(defun calcularCiclo()
+			(+ 90 6 120))
+
+(defun rangoOptimo()
+			(if (and (> (calcularCiclo) 35)
+					 (< (calcularCiclo) 150))
+				(print "Esta en el rango optimo")
+				(print "No esta en el rango optimo")))
+
 #|;Requerimiento 5: Planificación Temporal;
 ;; ========================================================
 ;; FUNCIÓN: timer
@@ -70,15 +87,19 @@
 
 (defun intervalosCiclo(inicio)
 				(cond
-				((and(< inicio 90)(>(- (mod 3600 216) (- 90 inicio) 6)120)) (list (+ (- 90 inicio) (- (mod 3600 216) (- 90 inicio)6 120))6 120)) ; Arranca en rojo y el verde excede 120,
-				((< inicio 90) (list (- 90 inicio) 6 (- (mod 3600 216) (- 90 inicio) 6)))														 ; Arranca en rojo normal
-				((< inicio 96) (list (- (mod 3600 216)(- 96 inicio) 120)(- 96 inicio) 120))														 ; Arranca en amarillo normal
-				((and(>= inicio 96) (> (- (mod 3600 216) (- 216 inicio)) 90))(list 90 6 (+ (- 216 inicio) (- 144 (- 216 inicio) 90 6))))		 ; Arranca en verde y el rojo excede 90
-				(t (list(- (mod 3600 216) (- 216 inicio)) 0 (- 216 inicio)))))																	 ; Arranca en verde normal
+				((and(< inicio 90)(>(- (mod 3600 216) (- 90 inicio) 6)120)) 			;Inicia en rojo y el verde excede 120
+				(list (+ (- 90 inicio) (- (mod 3600 216) (- 90 inicio)120 6))120 6)) 
+				((< inicio 90) 															;Inicia en rojo normal
+				(list (- 90 inicio) 120 (- (mod 3600 216) (- 90 inicio) 120)))				 
+				((< inicio 210)															;Inicia en verde normal
+				 (list (- (mod 3600 216)(- 210 inicio) 6)(- 210 inicio) 6))			
+				((and(>= inicio 210) (> (- (mod 3600 216) (- 216 inicio)) 90))			;Inicia en amarillo y el rojo excede 90
+				 (list 90 (- (mod 3600 216) (- 216 inicio) 90) (- 216 inicio)))		 
+				(t (list(- (mod 3600 216) (- 216 inicio)) 0 (- 216 inicio)))))			;Inicia en amarillo
 
 (defun mostrarPorcentajes(hora-unix)
 			(list
-      (porcentajeColor (+ (* 16 90) (car (intervalosCiclo (mod hora-unix 216)))))
-      (porcentajeColor (+ (* 16 6) (cadr (intervalosCiclo (mod hora-unix 216)))))
-      (porcentajeColor (+ (* 16 120) (caddr (intervalosCiclo (mod hora-unix 216)))))))
+      'rojo (porcentajeColor (+ (* 16 90) (car (intervalosCiclo (mod hora-unix 216)))))
+      'verde (porcentajeColor (+ (* 16 120) (cadr (intervalosCiclo (mod hora-unix 216)))))
+      'amarillo (porcentajeColor (+ (* 16 6) (caddr (intervalosCiclo (mod hora-unix 216)))))))
 
