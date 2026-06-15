a6dc8408b54209de613c0d6fa878bde02d27f74a
santikszxg <santi.ortiz001@gmail.com>
2026-06-15 01:52:41 -0300

    Update solucion.ml

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -41,3 +41,48 @@ val prueba2 : string = "amarillo"
 val prueba3 : string = "rojo"
 val prueba4 : string = "rojo"
 val prueba5 : string = "verde" 
+
+
+
+type color = EnRojo |  Verde | Rojo | EnVerde | Amarillo |EnAmarillo | RojoIntermitente | VerdeIntermitente | AmarilloIntermitente
+
+
+let transicion coloractual cambiara =
+ match (coloractual, cambiara) with
+| (EnRojo, Verde) -> (coloractual,"cambiar a verde")
+| (EnVerde, Amarillo) -> (coloractual, "cambiar a amarillo")
+| (EnAmarillo, Rojo) -> (coloractual, "cambiar a rojo")
+| (EnRojo, RojoIntermitente) -> (coloractual, "cambiar a rojo intermitente")
+| (EnVerde,VerdeIntermitente) -> (coloractual, "cambiar a verde intermitente")
+| (EnAmarillo, AmarilloIntermitente) -> (coloractual, "cambiar a amarillo intermitente")
+| _ -> (coloractual, "Accion por defecto")
+
+
+let prueba1 = transicion EnVerde Amarillo
+let prueba2 = transicion EnRojo Verde
+let prueba3 = transicion Verde Verde
+let prueba4 = transicion EnAmarillo Rojo
+let prueba5 = transicion EnRojo RojoIntermitente
+let prueba6 = transicion EnVerde VerdeIntermitente
+let prueba7 = transicion EnAmarillo  AmarilloIntermitente
+
+
+let timer tiempo_unix =
+ if(tiempo_unix mod 216) <= 86  then "rojo"
+else if (tiempo_unix mod 216) <= 89 then "rojo intermitente"
+else if (tiempo_unix mod 216) <= 206 then "verde"
+else if (tiempo_unix mod 216) <= 209 then "verde intermitente"
+else if (tiempo_unix mod 216) <= 212 then "amarillo"
+else "amarillo intermitente"
+
+let prueba1 = timer 500
+let prueba2 = timer 215
+let prueba3 = timer 212
+let prueba4 = timer 100
+
+val timer : int -> string = <fun>
+val prueba1 : string = "rojo"
+val prueba2 : string = "amarillo intermitente"
+val prueba3 : string = "amarillo"
+val prueba4 : string = "verde"
+
