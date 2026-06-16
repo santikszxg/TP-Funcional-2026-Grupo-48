72c94f70258540ca96a5d4ca533c64019eebc21d
santikszxg <santi.ortiz001@gmail.com>
2026-06-15 22:27:43 -0300

    Update config.localtime.lisp

--- lisp/config.localtime.lisp
+++ lisp/config.localtime.lisp
@@ -1,3 +1,7 @@
+(load "ruta del quicklisp")
+(ql:quickload "local-time")
+
+
 (defun auditoria (color-anterior color-nuevo)
   (format t "Tiempo ~A: La luz ha cambiado de ~A a ~A~%" (local-time:format-timestring nil (local-time:now) :format '(:year "-" :month "-" :day " " :hour ":" :min ":" :sec))
 (car (transicion color-anterior color-nuevo)) (cadr(transicion color-anterior color-nuevo))))
