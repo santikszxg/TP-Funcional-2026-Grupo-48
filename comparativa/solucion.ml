98ee935b8bb8dd85c37bef379d48ca1ae4c199eb
santikszxg <santi.ortiz001@gmail.com>
2026-06-16 20:29:44 -0300

    Update solucion.ml

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -1,6 +1,12 @@
 
+(*
+========================================================
+   FUNCIÓN: transicion
+   NATURALEZA: Pura
+   ESTRATEGIA: Busca un patron mediante constructores
+   IMPACTO: No destructiva
+   ======================================================== *)
 type color = EnRojo |  Verde | Rojo | EnVerde | Amarillo |EnAmarillo 
-
 
 let transicion coloractual cambiara =
  match (coloractual, cambiara) with
@@ -21,8 +27,13 @@ val prueba1 : color * string = (EnVerde, "cambiar a amarillo")
 val prueba2 : color * string = (EnRojo, "cambiar a verde")
 val prueba3 : color * string = (Verde, "Accion por defecto")
 val prueba4 : color * string = (EnAmarillo, "cambiar a rojo")
-
-
+(*
+========================================================
+   FUNCIÓN: timer
+   NATURALEZA: Pura 
+   ESTRATEGIA: Función predicado (Evalúa condiciones lógicas con estructuras if/else)
+   IMPACTO: No destructiva
+   ======================================================== *)
 
 let timer tiempo_unix =
  if(tiempo_unix mod 216) <= 90  then "rojo"
@@ -42,7 +53,13 @@ val prueba3 : string = "rojo"
 val prueba4 : string = "rojo"
 val prueba5 : string = "verde" 
 
-
+(*
+========================================================
+   FUNCIÓN: transicion
+   NATURALEZA: Pura
+   ESTRATEGIA: Busca un patron mediante constructores
+   IMPACTO: No destructiva
+   ======================================================== *)
 
 type color = EnRojo |  Verde | Rojo | EnVerde | Amarillo |EnAmarillo | RojoIntermitente | VerdeIntermitente | AmarilloIntermitente
 
@@ -66,7 +83,13 @@ let prueba5 = transicion EnRojo RojoIntermitente
 let prueba6 = transicion EnVerde VerdeIntermitente
 let prueba7 = transicion EnAmarillo  AmarilloIntermitente
 
-
+(*
+========================================================
+   FUNCIÓN: timer
+   NATURALEZA: Pura 
+   ESTRATEGIA: Función predicado (Evalúa condiciones lógicas con estructuras if/else)
+   IMPACTO: No destructiva
+   ======================================================== *)
 let timer tiempo_unix =
  if(tiempo_unix mod 216) <= 86  then "rojo"
 else if (tiempo_unix mod 216) <= 89 then "rojo intermitente"
